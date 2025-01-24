39fe3ddedda6c17c308b24661558b7e08eb04436
Станислав Маяцкий <stanislavmayatsky@192.168.10.22>
2025-01-24 22:47:53 +0500

    Implemented settings in main/urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 """
+main/urls.py
 URL configuration for config project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("postpilot/", include("postpilot.urls", namespace="postpilot"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
