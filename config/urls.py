49f31c433dce5da033037dac60e0fe26b40eb191
Станислав Маяцкий <stanislavmayatsky@192.168.10.22>
2025-01-27 00:12:47 +0500

    Implemented migrates for models and simple fixtures

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("postpilot/", include("postpilot.urls", namespace="postpilot"))
+    # path("postpilot/", include("postpilot.urls", namespace="postpilot"))
 ]
 
 if settings.DEBUG:
