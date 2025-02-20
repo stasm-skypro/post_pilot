ed4f807685dc5f37751f63d71a07c9894588656f
Станислав Маяцкий <stasm226@gmail.com>
2025-02-20 19:41:56 +0500

    Add mailings total count on Mailinga Card and create button to create mailing

--- postpilot/urls.py
+++ postpilot/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .apps import PostpilotConfig
-from .views import HomeView, WelcomeView
+from .views import HomeView, WelcomeView, MailingListView
 
 app_name = PostpilotConfig.name
 
 urlpatterns = [
     path("", WelcomeView.as_view(), name="welcome"),  # Главная страница приложения
     path("home/", HomeView.as_view(), name="home"),
+    path("mailings/", MailingListView.as_view(), name="mailing_list"),
 ]
