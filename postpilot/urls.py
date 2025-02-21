4d879980a587904d665d87925fc13222c29b5dc0
Станислав Маяцкий <stasm226@gmail.com>
2025-02-21 15:24:53 +0500

    Refactores: welcome template get on center, add create mailing button and mailing form

--- postpilot/urls.py
+++ postpilot/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from .apps import PostpilotConfig
-from .views import HomeView, WelcomeView, MailingListView
+from .views import HomeView, WelcomeView, MailingCreateView
 
 app_name = PostpilotConfig.name
 
 urlpatterns = [
     path("", WelcomeView.as_view(), name="welcome"),  # Стартовая страница приложения
     path("home/", HomeView.as_view(), name="home"),   # Главная страница приложения
-    path("mailings/", MailingListView.as_view(), name="mailing_list"),
+    path("mailing_form/", MailingCreateView.as_view(), name="mailing_create"),  # Форма создания рассылки
 ]
